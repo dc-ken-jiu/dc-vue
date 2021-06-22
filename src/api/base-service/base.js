762b573d60b4c34110f81d528b5f88d47735bb68
丁学紧 <accounts_5a503b085f65ba798cb5b115@mail.teambition.com>
2021-06-22 14:55:28 +0800

    fix:修复eslint报错问题

--- src/api/base-service/base.js
+++ src/api/base-service/base.js
@@ -2,12 +2,12 @@ import axios from '../../utils/axios/config';
 import { Message } from 'element-ui';
 
 export const basePost = (url, value) => {
-  return axios.post(url, value).catch(err => {
+  return axios.post(url, value).catch((err) => {
     Message.error(err.message);
   });
 };
 export const baseGet = (url, value) => {
-  return axios.get(url, value).catch(err => {
+  return axios.get(url, value).catch((err) => {
     Message.error(err.message);
   });
 };
